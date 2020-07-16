8e3151da584d12f25a595524d920028a9a63593d
p6l-richard <richard.poelder@gmail.com>
2020-07-16 15:42:47 +0200

    Bug: Remove drop_tables() again for production

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ def setup_db(app, db_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.drop_all()
+    # db.drop_all()
     # db.create_all()
 
 
